3df2fde747bfed488fc3c0c2ef4f45700dd4fc0f
FatBird-Gu <53741837+FatBird-Gu@users.noreply.github.com>
2020-03-16 11:51:47 +0800

    V3.8 add function of sending message(Controller ajax)

--- src/main/resources/static/js/letter.js
+++ src/main/resources/static/js/letter.js
@@ -5,6 +5,30 @@ $(function(){
 
 function send_letter() {
 	$("#sendModal").modal("hide");
+
+	var toname = $("#recipient-name").val();
+	var content = $("#message-text").val();
+	$.post(
+		CONTEXT_PATH+"/letter/send",
+		{"toName":toname,"content":content},
+		function (data) {
+			data = $.parseJSON(data);
+			if (data.code == 0){
+				$("#hintBody").text("发送成功!");
+			} else{
+				$("#hintBody").text(data.msg);
+			}
+
+			$("#hintModal").modal("show");
+			setTimeout(function(){
+				$("#hintModal").modal("hide");
+				location.reload();
+			}, 2000);
+
+		}
+	)
+
+
 	$("#hintModal").modal("show");
 	setTimeout(function(){
 		$("#hintModal").modal("hide");
