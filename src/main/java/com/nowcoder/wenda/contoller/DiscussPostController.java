6802ce955d0d742950019729e558c0216a0c2f9b
FatBird-Gu <53741837+FatBird-Gu@users.noreply.github.com>
2020-03-19 11:26:20 +0800

    V4

--- src/main/java/com/nowcoder/wenda/contoller/DiscussPostController.java
+++ src/main/java/com/nowcoder/wenda/contoller/DiscussPostController.java
@@ -7,6 +7,7 @@ import com.nowcoder.wenda.entity.Page;
 import com.nowcoder.wenda.entity.User;
 import com.nowcoder.wenda.service.CommentService;
 import com.nowcoder.wenda.service.DiscussPostService;
+import com.nowcoder.wenda.service.LikeService;
 import com.nowcoder.wenda.service.UserService;
 import com.nowcoder.wenda.util.HostHolder;
 import com.nowcoder.wenda.util.WendaConstant;
@@ -34,6 +35,8 @@ public class DiscussPostController implements WendaConstant{
     private UserService userService;
     @Autowired
     private CommentService commentService;
+    @Autowired
+    private LikeService likeService;
 
     @RequestMapping(value = "/add",method = RequestMethod.POST)
     @ResponseBody
@@ -62,6 +65,13 @@ public class DiscussPostController implements WendaConstant{
         User user = userService.findUserById(post.getUserId());
         model.addAttribute("user",user);
 
+        // 点赞数量
+        long likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_POST, discussPostId);
+        model.addAttribute("likeCount",likeCount);
+        // 点赞状态
+        int likeStatus = hostHolder.getUser() == null ? 0:likeService.findEntityLikeStatus(hostHolder.getUser().getId(), ENTITY_TYPE_POST, discussPostId);
+        model.addAttribute("likeStatus", likeStatus);
+
         // 评论的配置信息
         page.setLimit(5);
         page.setPath("/discuss/detail/" + discussPostId);
@@ -82,6 +92,13 @@ public class DiscussPostController implements WendaConstant{
                 commentVO.put("comment",comment);
                 // 评论作者
                 commentVO.put("user",userService.findUserById(comment.getUserId()));
+                // 点赞数量
+                likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_COMMENT, comment.getId());
+                commentVO.put("likeCount",likeCount);
+                // 点赞状态
+                likeStatus = hostHolder.getUser() == null ? 0:
+                        likeService.findEntityLikeStatus(hostHolder.getUser().getId(), ENTITY_TYPE_COMMENT, comment.getId());
+                commentVO.put("likeStatus", likeStatus);
                 // 回复列表
                 List<Comment> replyList = commentService.findCommentsByEntity(
                         ENTITY_TYPE_COMMENT, comment.getId(),0,Integer.MAX_VALUE);
@@ -94,6 +111,13 @@ public class DiscussPostController implements WendaConstant{
                         replyVo.put("reply",reply);
                         // 作者
                         replyVo.put("user",userService.findUserById(reply.getUserId()));
+                        // 点赞数量
+                        likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_COMMENT, reply.getId());
+                        replyVo.put("likeCount",likeCount);
+                        // 点赞状态
+                        likeStatus = hostHolder.getUser() == null ? 0:
+                                likeService.findEntityLikeStatus(hostHolder.getUser().getId(), ENTITY_TYPE_COMMENT, reply.getId());
+                        replyVo.put("likeStatus", likeStatus);
                         // 回复目标
                         User target = reply.getTargetId() == 0? null :userService.findUserById(reply.getTargetId());
                         replyVo.put("target",target);
