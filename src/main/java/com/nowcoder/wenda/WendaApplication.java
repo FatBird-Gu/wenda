307ff4d925f5fd64a63ec1fcc0a34aef4933ce06
FatBird-Gu <53741837+FatBird-Gu@users.noreply.github.com>
2020-03-22 13:41:52 +0800

    V6 elasticsearch Test

--- src/main/java/com/nowcoder/wenda/WendaApplication.java
+++ src/main/java/com/nowcoder/wenda/WendaApplication.java
@@ -3,9 +3,17 @@ package com.nowcoder.wenda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class WendaApplication {
 
+	@PostConstruct
+	public void init(){
+		// 解决netty启动冲突问题
+		// see netty4Utils
+		System.setProperty("es.set.netty.runtime.available.processors","false");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(WendaApplication.class, args);
 	}
